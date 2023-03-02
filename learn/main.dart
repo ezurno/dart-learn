32e77661b634eba7a71f78fd1c747107e8137250
ezurno <ezurno@gmail.com>
2023-03-02 22:50:39 +0900

    LEARN: default types
    1. var
     - ts, js 에서 쓰던 그 var
     - 타입을 명시하지 않을 수 있음
     - 대신 부여한 value type과 다른 type값을 넣으면 error
    
    2. String, int . . .
     - 타입을 명시해서 사용할 수도 있음
    
    3. dynamic type
     - dynamic은 var와는 비슷하지만 다름
     - var는 한번 값이 들어가면 다른 type은 사용이 불가능 한것과는 달리 타입 자체를 정하지 않았기 때문에 동적으로 계속 바뀜

--- learn/main.dart
+++ learn/main.dart
@@ -1,3 +1,12 @@
 void main() {
-  print("Hello world!");
+  var name = "LEE"; // var로 타입을 명시하지 않을 수 있음
+  String word = "Hello"; // 반대로 type을 명시 해 줄 수도 있음
+
+  dynamic dyn;
+  dyn = 'lee';
+  dyn = 123;
+  dyn = false;
+  // dynamic 은 type을 명시해 주지 않아도 유동적으로 바뀌는 타입
+  // dynamic 은 var와는 조금 다름
+  // var 는 type이 바뀌면 error가 뜨지만 dynamic은 뜨지 않음
 }
