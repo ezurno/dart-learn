4b6f41037f763167966b10c2b62e97a85963bb41
ezurno <ezurno@gmail.com>
2023-03-03 19:48:23 +0900

    LEARN: late, const
    1. late
     - API에서 값을 받아올때 잠시 대기
     - 값을 final에서 먼저 적용 시키지 않아도 가능
    
    2. const
     - js ts와는 다르게 comfile 기준임
     - comfile 기준에서 값이 없으면 error.
     - API 통신때 const를 사용하면 안됨 (late final 을 사용한다)

--- learn/main.dart
+++ learn/main.dart
@@ -1,13 +1,16 @@
 void main() {
-  // final var name = "Name"; error!! var에는 final 사용 불가
-  const constName = "name"; // const 사용하면 됨
+  late final String name; // 값이 들어올때까지 잠시 대기
+  // 값이 아직 들어오지 않았지만 final로 지정할 수 있으며 API와 소통할때 유용함
 
-  final String name = "name";
-  // final을 사용 하면 name을 변경하지 못하게 할 수 있음
+  name = "Hello";
 
-  /*------------------*/
+  /*------*/
 
-  String? isNull = "word";
-  isNull = null;
-  print(isNull?.length); // null safety
+  final APIfinal = fetchAPI();
+  const APIconst =
+      fetchAPI(); // error. Dart 에서의 const는 다른 외부 함수에서 가져오는 value를 사용할때 사용 x
+}
+
+String fetchAPI() {
+  return "123";
 }
