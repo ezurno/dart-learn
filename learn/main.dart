2df8092b1908c99fc4b5ac669b2c240560ba7e81
ezurno <ezurno@gmail.com>
2023-03-03 18:33:56 +0900

    LEARN: final, null safety
    1. final 로 값 수정 불가 지정 가능
    
    2. null safety는 떄로는 null 값이 되게끔 지정 가능 String? value?.method()

--- learn/main.dart
+++ learn/main.dart
@@ -1,12 +1,13 @@
 void main() {
-  var name = "LEE"; // var로 타입을 명시하지 않을 수 있음
-  String word = "Hello"; // 반대로 type을 명시 해 줄 수도 있음
+  // final var name = "Name"; error!! var에는 final 사용 불가
+  const constName = "name"; // const 사용하면 됨
 
-  dynamic dyn;
-  dyn = 'lee';
-  dyn = 123;
-  dyn = false;
-  // dynamic 은 type을 명시해 주지 않아도 유동적으로 바뀌는 타입
-  // dynamic 은 var와는 조금 다름
-  // var 는 type이 바뀌면 error가 뜨지만 dynamic은 뜨지 않음
+  final String name = "name";
+  // final을 사용 하면 name을 변경하지 못하게 할 수 있음
+
+  /*------------------*/
+
+  String? isNull = "word";
+  isNull = null;
+  print(isNull?.length); // null safety
 }
