95da731dba023121db7b58ab7204ef80ef3ada02
ezurno <ezurno@gmail.com>
2023-03-03 21:37:38 +0900

    LEARN: string interpolation
    1. string을 합칠 때 ${} 으로 바로 붙여서 사용 가능
    
    2. $만 사용해도 되고 ${}로 사용 해도 됨 다만 계산식으로 사용 할땐 ${}

--- learn/main.dart
+++ learn/main.dart
@@ -1,19 +1,8 @@
 void main() {
-  List<int> number = [1, 2, 3, 4, 5];
-  // List<> 로 타입을 명시해 해당 타입의 배열만 만드는 방법
-  number.add(1);
-  print(number);
+  // String Interpolation
+  var name = "Lee";
+  var year = 2023;
+  var greeting = "Hello!! $name, ${year + 2}";
 
-  var list = [1, false, 3, 4, "sdf"];
-  // var type으로 배열을 만들면 type 상관없이 들어감
-
-  var trigger = true;
-  List<int> collection = [
-    1,
-    2,
-    3,
-    4,
-    if (trigger) 5,
-  ]; // trigger if로써 list 내에 if 문을 사용 할 수 있음
-  print((collection));
+  print(greeting);
 }
