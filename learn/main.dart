db620a5af20616a66dc15c58e55ef68c2cd276ce
ezurno <ezurno@gmail.com>
2023-03-03 22:22:28 +0900

    LEARN: Map<>
    1. Map<>은 TS의 object와 같으며 Dart의 object 형식은 TS의 any라고 볼 수 있다.
    
    2. type을 명시하지 않고 var를 사용하면 자체적으로 타입을 체크해서 봄
    
    3. API를 이용 할 때 List와 Map을 사용 하는 것 보단 후에 배울 class를 사용하길 권장

--- learn/main.dart
+++ learn/main.dart
@@ -1,10 +1,27 @@
 void main() {
-  // collection for
-  var oldFriends = ["Lee", "Kim"];
-  var newFriends = [
-    "Boo",
-    "Park",
-    "Nam",
-    for (var friend in oldFriends) "$friend ⭐"
-  ];
+  // map
+  // dart 에서의 map 은 ts 의 object, object는 ts의 any
+
+  var player = {
+    "name": "Lee",
+    "age": 27,
+  }; // type을 명시하지 않은 map
+
+  Map<int, bool> test = {
+    1: true,
+    2: false,
+  }; // type을 Map<>으로 명시한 map
+
+  List<Map<String, bool>> flavorList = [
+    {
+      "Apple": true,
+      "Abocado": false,
+    },
+    {
+      "Cheese": true,
+      "Milk": true,
+      "Yogurt": true,
+      "Cream": false,
+    },
+  ]; // API 에서 값을 가져올 땐 List와 Map을 이중으로 사용하는 것 보다 후에 배울 class를 이용 하는걸 추천
 }
