554dd8ba18e148b235843af9f6327242b53af37e
ezurno <ezurno@gmail.com>
2023-03-04 19:14:38 +0900

    LEARN: named parameter
    1. parameter(argument) 에 이름을 정하는 방법으로 하는걸 권장
    
    2. required, default value, ? 를 적절히 이용해 사용

--- learn/main.dart
+++ learn/main.dart
@@ -1,14 +1,18 @@
 void main() {
-  // set
-
-  var players = {"Lee", "Kim", "Park"}; // type이 명시되지 않은 set
-
-  Set<int> numbers = {1, 2, 3}; // type이 명시 된 set
-  numbers.add(4);
-  numbers.add(4);
-  numbers.add(4);
-  numbers.add(4);
-  numbers.add(4);
-
-  print(numbers); // Set의 값은 List와 달리 unique 하기 떄문에 값이 중복되면 하나만 들어감
+  print(sayHello("Lee", "EU"));
+  print(sayBye(name: "Kim", country: "KR"));
+  print(sayBye(name: "Park", number: 2016));
 }
+
+String sayHello(String name, String country) {
+  return "Hello! $name $country";
+} // 이렇게 사용을 하면 args의 첫번째 값이 무엇인지, 두번째 값이 무엇인지 헷갈림
+// 따라서 하단의 방법 사용 권장
+
+String sayBye({
+  required String name,
+  int number = 2022,
+  String? country,
+}) {
+  return "Bye, $name $country in $number";
+} // 순서가 달라져도 args 값을 입력하면 잘 들어감 단 함수 내 {} 사용
