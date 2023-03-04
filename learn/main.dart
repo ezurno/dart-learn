055c4eb34e134b397727bbe2cdbd052c63e66a37
ezurno <ezurno@gmail.com>
2023-03-04 19:50:55 +0900

    LEARN: optional positional parameter
    1. []로 없어도 되는 값을 감싸주고 ? 을 써주며 default 값도 넣어줌
    
    2. 개인적으로 named params를 사용하는게 더 간결하고 좋아보임

--- learn/main.dart
+++ learn/main.dart
@@ -1,18 +1,9 @@
 void main() {
-  print(sayHello("Lee", "EU"));
-  print(sayBye(name: "Kim", country: "KR"));
-  print(sayBye(name: "Park", number: 2016));
+  print(sayHello("Lee", "KR"));
 }
 
-String sayHello(String name, String country) {
-  return "Hello! $name $country";
-} // 이렇게 사용을 하면 args의 첫번째 값이 무엇인지, 두번째 값이 무엇인지 헷갈림
-// 따라서 하단의 방법 사용 권장
+String sayHello(String name, String country, [int? number = 2023]) {
+  return "Hello! $name, $country in $number";
+} // optional positional parameter
 
-String sayBye({
-  required String name,
-  int number = 2022,
-  String? country,
-}) {
-  return "Bye, $name $country in $number";
-} // 순서가 달라져도 args 값을 입력하면 잘 들어감 단 함수 내 {} 사용
+// [] 로 없어도 되는 값을 감싸줌, 하지만 이방법보다 named params 를 쓰는게 더 편함
