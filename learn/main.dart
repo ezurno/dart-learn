f142eddbfa9fd2a1e37b6528d0239eab4a4c2cc8
ezurno <ezurno@gmail.com>
2023-03-04 20:20:28 +0900

    LEARN: ??, ??=
    1. ??
     - ?? 연산자는 전자가 null 값이면 후자를 반환해 줌
    
    2. ??=
     - 만약 값이 null 이면 값을 할당

--- learn/main.dart
+++ learn/main.dart
@@ -1,9 +1,17 @@
 void main() {
-  print(sayHello("Lee", "KR"));
+  print(reformer("lee"));
+  print(reformer(null));
+
+  String? test;
+  test ??= "Hello"; // test가 null 이면 값을 할당
+  print(test);
 }
 
-String sayHello(String name, String country, [int? number = 2023]) {
-  return "Hello! $name, $country in $number";
-} // optional positional parameter
-
-// [] 로 없어도 되는 값을 감싸줌, 하지만 이방법보다 named params 를 쓰는게 더 편함
+String? reformer(String? word) {
+  return word?.toUpperCase() ?? null;
+}
+/*
+  1. STring? 으로 return 값이 null이 될 수 있음을 알림
+  2. args의 String? 값으로 args 가 null이 될 수 있음
+  3. ?? 로 전자가 null 이면 후자의 값 return
+*/
