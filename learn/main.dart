448c0eaee391b8cc7d3e02cbd09df36c6fc6eb58
ezurno <ezurno@gmail.com>
2023-03-05 16:45:46 +0900

    LEARN: enum
    1. enum 은 value 값을 직접 입력하다보면 나는 오타로 인한 에러발생을 줄이는 역할
    
    2. enum type을 걸어주면 enum의 value 값만 사용 할 수 있게 됨 => 오타 방지

--- learn/main.dart
+++ learn/main.dart
@@ -1,13 +1,17 @@
+enum Country { KR, EU } // value의 값을 직접 입력하면 오타가 날 가능성이 생김
+// enum을 사용해 값을 정형화 시켜놓음
+
 void main() {
-  var lee = User(name: "Lee", country: "KR", old: 27)
-    ..country = "EU"
+  var lee = User(name: "Lee", country: Country.KR, old: 27)
+    ..country = Country.EU
     ..old = 26; // cascade notation
 
   print(lee.old);
 }
 
 class User {
-  String name, country;
+  String name;
+  Country country; // enum type을 걸어놓으면 country 의 값은 enum 의 value만 사용이 가능함
   int old;
 
   User({
