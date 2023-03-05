b5d795c3938ebace4b249fe63034f6bbe09ba590
ezurno <ezurno@gmail.com>
2023-03-05 16:19:58 +0900

    LEARN: cascade notation
    1. 객체로 생성한 값의 변수를 수정하고 싶을땐 object.variable 으로 수정하는 것 보단 cascade notation을 사용하는것이 편함
    
    2. .. 으로 특정 객체의 변수에 접근 함

--- learn/main.dart
+++ learn/main.dart
@@ -1,42 +1,20 @@
 void main() {
-  var apiData = [
-    {
-      "name": "Lee",
-      "country": "KR",
-      "old": 27,
-    },
-    {
-      "name": "Kim",
-      "country": "KR",
-      "old": 26,
-    },
-    {
-      "name": "Park",
-      "country": "KR",
-      "old": 25,
-    },
-  ]; // 외부 api 에서 가져왔다는 예시
+  var lee = User(name: "Lee", country: "KR", old: 27)
+    ..country = "EU"
+    ..old = 26; // cascade notation
 
-  apiData.forEach((userJson) {
-    var user = User.fromJson(userJson); // 각 값을 User class로 객체 생성
-    user.userInfo();
-  });
+  print(lee.old);
 }
 
 class User {
-  late final String name, country;
-  late final int old;
+  String name, country;
+  int old;
 
   User({
     required this.name,
     required this.country,
     required this.old,
   });
-
-  User.fromJson(Map<String, dynamic> userJson)
-      : name = userJson["name"],
-        country = userJson["country"],
-        old = userJson["old"];
 
   void userInfo() {
     print("$name , $country, $old");
